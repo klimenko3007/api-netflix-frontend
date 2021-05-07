6f15149be7e2f46e205cdb74afe16fcde609f037
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-05-07 23:22:40 +0200

    Styled home screen

--- src/components/TVShowsComponent.js
+++ src/components/TVShowsComponent.js
@@ -5,12 +5,14 @@ import TVShowThumb from './TVShowThumb'
 import { SHOWS_PER_PAGE } from '../reusables/constants'
 
 const TVShowsComponent = ({ tvShows, pageNumber }) => {
-  const startIndex = (pageNumber - 1)*SHOWS_PER_PAGE
-  const selectedShows = tvShows.slice(startIndex, startIndex+SHOWS_PER_PAGE)
+  const startIndex = (pageNumber - 1) * SHOWS_PER_PAGE
+  const selectedShows = tvShows.slice(startIndex, startIndex + SHOWS_PER_PAGE)
 
   return (
     <div>
-      <h2>{selectedShows.length === 0? "Nothing matching your choice" :"Here are your choices"}</h2>
+      <h2 className="section-subheading">
+        {selectedShows.length === 0 ? "Nothing matching your choice" : "Here are your choices"}
+      </h2>
       {selectedShows.map(show => <TVShowThumb key={show.show_id} show={show} />)}
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>
-      <p>
-        Home component
-      </p>
+    <div
+      className="home-contaier"
+      style={{ backgroundImage: `url('./assets/home_background.jpg')` }}
+    >
+      <h1 className="headline-home">
+        Welcome to Netflix data set!
+      </h1>
+      <h2 className="subheading-home">
+        Check out the information about Netflix TV shows and movies!
+      </h2>
+      <div className="home-wrapper">
+        <Link className="home-link" to="/TV-Shows">TV Shows</Link>
+        <Link className="home-link" to="/Movies">Movies</Link>
+      </div>
+
     </div>
   )
 }
