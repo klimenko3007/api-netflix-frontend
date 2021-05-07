4d6fab114521a2294444c318ca455b64a9b9b2f9
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-05-07 21:05:11 +0200

    Added Header and styled it

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import About from './pages/About'
 import Movies from './pages/Movies'
 import TVShows from './pages/TVShows'
 import ShowPage from './pages/ShowPage'
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
       <div className="content-container">
-        <nav className="nav-container">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/Movies">Movies</Link>
-            </li>
-            <li>
-              <Link to="/TV-Shows">TV Shows</Link>
-            </li>
-          </ul>
-        </nav>
+        <Header />
         <Switch>
           <Route path="/" exact>
             <Home />
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="nav-container">
+          <ul className="nav-container">
+            <li >
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/Movies">Movies</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/TV-Shows">TV Shows</Link>
+            </li>
+          </ul>
+        </nav>
+  )
+}
+export default Header
